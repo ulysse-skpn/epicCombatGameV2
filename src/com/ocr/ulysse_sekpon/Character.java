deb4557ba9beaea12bb1f50d95c6316c89f73acc
ulysse-spkn <u.sekpon@gmail.com>
2019-05-05 17:01:38 +0200

    Complétion de la classe Character /// Character class was completed

--- src/com/ocr/ulysse_sekpon/Character.java
+++ src/com/ocr/ulysse_sekpon/Character.java
@@ -1,4 +1,35 @@
 package com.ocr.ulysse_sekpon;
 
-public class Character {
+public abstract class Character {
+
+    int level, health, strength, agility, intelligence;
+
+    String playerName, word, characterClass;
+
+    Character opponent;
+
+
+    Character(int nbPlayer, int strength, int agility, int intelligence) {
+        playerName = "Joueur " + nbPlayer;
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+        level = strength + agility + intelligence;
+        health = 5*level;
+    }
+
+    public abstract String game(int actionToDo);
+    public void Opponent(Character opponent) {
+        this.opponent = opponent;
+    }
+
+
+    public String characterSummary() {
+        return word + " ! Je suis le " + characterClass + " " + playerName + ", niveau " + level + ", je possède " + health + " de vitalité, " + strength + " de force, " + agility + " d'agilité et " + intelligence + " d'intelligence !";
+    }
+
+
+
+    public void healthDecrease(int healthToSubstract) { health = health - healthToSubstract; }
+
 }
